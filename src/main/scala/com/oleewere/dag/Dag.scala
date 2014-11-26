54ccf8be98ebec1c933b0e543e840042fe00f306
oleewere <oleewere@gmail.com>
2014-11-26 16:44:46 +0100

    added dag iterator

--- src/main/scala/com/oleewere/dag/Dag.scala
+++ src/main/scala/com/oleewere/dag/Dag.scala
@@ -1,5 +1,24 @@
 package com.oleewere.dag
 
+import scala.collection.immutable.Stack
+
 class Dag[T](val root : Node[T])
 
-class Node[T](val parents: List[Node[T]], val children: List[Node[T]], val props: Map[String, Any])
+class Node[T](val parents: List[Node[T]], val children: List[Node[T]], val value : T)
+
+class DagIterator[T](val dag: Dag[T]) extends Iterator {
+
+  val nodes : Stack[Node[T]] = new Stack[Node[T]]
+
+  nodes.push(dag.root)
+
+  override def hasNext: Boolean = !nodes.isEmpty
+
+  override def next(): T = {
+    val node = nodes.top
+    for (child <- node.children){
+      nodes.push(child);
+    }
+    node.value
+  }
+}
